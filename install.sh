712c8ae6c140dcf35241dd3d28255cfffbc8f8ad
Reuben  F <63928521+railwhale@users.noreply.github.com>
2022-08-26 10:02:00 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 # To install only the user-specific files, use nome-install.sh
-printf "Files retrived\n"
+printf "Files retrived\n\n"
+
 echo This will replace your .bashrc, .profile or .bash_login, or bash_profile, and create .bash_aliases, and .bash_config
 echo /etc/bash.bashrc, and /etc/profile will also be replaced
 echo Replaced files will be moved to $HOME/old-bash-config
+echo
 
 read -p "Please confirm install [y/n] " -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [ $REPLY =~ ^[Yy]$ ]; then
   cd
   mkdir old-bash-config
   mv ~/.bashrc ~/old-bash-config
