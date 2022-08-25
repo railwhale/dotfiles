030d111869885e43a9a88a0a77009cea0d3c485a
Reuben  F <63928521+railwhale@users.noreply.github.com>
2022-08-26 08:33:00 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,1 @@
-# Run this install script to install all these dotfiles on a linux system
 
-# IMPORTANT NOTICE: this script will remove and replace any and all files
-# with the same name in the same path, so make sure you you know what you are replacing
-
-# If you want to only install the user-specific files, use home-install.sh
-
-cd # this script will run from your home
