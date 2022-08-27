14e5feee8551578b964302d3e44570430d0810ac
Reuben  F <63928521+railwhale@users.noreply.github.com>
2022-08-27 17:34:45 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,12 @@
 # To install only the user-specific files, use nome-install.sh
-printf "Files retrived\n\n"
+clear
 
-echo This will replace your .bashrc, .profile or .bash_login, or bash_profile, and create .bash_aliases, and .bash_config
+echo This will replace your .bashrc, .profile or .bash_login, or bash_profile, and create or replace .bash_aliases, and .bash_config
 echo /etc/bash.bashrc, and /etc/profile will also be replaced
 echo Replaced files will be moved to $HOME/old-bash-config
 echo
 
-cd
-mkdir old-bash-config
-mv ~/.bashrc ~/old-bash-config
-
-if [ -f ~/.bash_profile ]; then
-  mv ~/.bash_profile ~/old-bash-config
-fi
-
-if [ -f ~/.bash_login ]; then
-  mv ~/.bash_login ~/old-bash-config
-fi
-
-if [ -f ~/.profile ]; then
-  mv ~/.profile ~/old-bash-config
-fi
-
-if [ -f ~/.bash_aliases ]; then
-  mv ~/.bash_aliases ~/old-bash-config
-fi
-
-if [ -f ~/.bash_config ]; then
-  mv ~/.bash_config ~/old-bash-config
-fi
+. ~/dotfiles/home-install.sh
 
 if [ -f /etc/profile ]; then
   mv /etc/profile ~/old-bash-config/etc-profile
@@ -38,11 +16,9 @@ if [ -f /etc/bash.bashrc ]; then
   mv /etc/bash.bashrc ~/old-bash-config/bash.bashrc
 fi
 
+shopt -s globstar
+cp -r /home/$USER/dotfiles/bash/home/** ~        # copy all files in dotfiles/bash/home (bashrc, bash aliases, more) to users home
+sudo cp -r /home/$USER/dotfiles/bash/etc/** /etc/     # copy all files in dotfile/bash/etc to /etc/
+cd
 
-cp /home/$USER/dotfiles/bash/~/* ~        # copy all files in dotfiles/bash/~ (bashrc, bash aliases, more) to users home
-cp /home/$USER/dotfiles/bash/etc/* /etc/   # copy all file
-
-read -p " Apply config to current session? [y/N] (screen will clear)" -r
-if [[ $REPLY = ^[Yy]$ ]]; then 
-  source .bashrc
-fi
+echo To apply these changes, restart your terminal emulator
